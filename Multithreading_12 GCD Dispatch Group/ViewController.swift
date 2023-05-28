993052dc93c6a0619e6dc6d70cceefe7fff8baec
TheViLkiS <thevilkis@gmail.com>
2023-05-28 11:59:01 +0400

    Initial Commit

---
+++ Multithreading_12 GCD Dispatch Group/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Multithreading_12 GCD Dispatch Group
+//
+//  Created by Дмитрий Гусев on 28.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
